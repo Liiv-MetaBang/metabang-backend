2a68bd4aaf153406ebd7c13ac18c424c7d39e6e4
0yeap95d <tjdwob@gmail.com>
2021-10-19 17:45:28 +0900

    bsj | swagger

---
+++ bin/README.md
@@ -0,0 +1,14 @@
+# User Repo
+
+localhost:8080/h2-console
+
+auto ddl sql 
+
+```
+insert into user(user_id, user_name, password,  account, email,role) values ( null, 'test' ,'1234' , null , null ,1);
+
+select * from user;
+```
+
+
+
--- src/main/java/com/dkne/metabang/web/UserController.java
+++ src/main/java/com/dkne/metabang/web/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dkne.metabang.service.UserService;
@@ -13,6 +15,7 @@ import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("v1")
 public class UserController {
 
 	private final UserService userService;
